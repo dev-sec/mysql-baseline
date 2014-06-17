b8797a4a70e05742be9719d4384d78904dc5b1b1
DI Edmund Haselwanter <office@iteh.at>
2014-06-17 13:02:05 +0200

    add OS-check

--- default/serverspec/mysql_spec.rb
+++ default/serverspec/mysql_spec.rb
@@ -6,6 +6,13 @@ RSpec.configure do |c|
   c.filter_run_excluding skipOn: backend(Serverspec::Commands::Base).check_os[:family]
 end
 
+case backend.check_os[:family]
+when 'Ubuntu'
+  mysql_config_file = '/etc/mysql/my.cnf'
+when 'RedHat', 'Fedora'
+  mysql_config_file = '/etc/my.cnf'
+end
+
 describe 'Mysql owner, group and permissions' do
 
   describe file('/etc/mysql') do
