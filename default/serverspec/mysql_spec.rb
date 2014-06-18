771423b13daed63ca85ac4eaaa65cecaa0a8398a
DI Edmund Haselwanter <office@iteh.at>
2014-06-18 11:07:28 +0200

    Req. 297,298,302

--- default/serverspec/mysql_spec.rb
+++ default/serverspec/mysql_spec.rb
@@ -13,6 +13,21 @@ when 'RedHat', 'Fedora'
   mysql_config_file = '/etc/my.cnf'
 end
 
+# Req. 297
+describe command("mysql -uroot -p#{ENV['mysql_password']} mysql -s -e 'show databases like \"test\";'") do
+  it { should return_stdout nil }
+end
+
+# Req. 298
+describe command("mysql -uroot -p#{ENV['mysql_password']} mysql -s -e 'select count(*) from mysql.user where user=\"\";' | tail -1") do
+  it { should return_stdout "0" }
+end
+
+# Req. 302
+describe file(mysql_config_file) do
+  it { should contain 'password' }
+end
+
 describe 'Mysql owner, group and permissions' do
 
   describe file('/etc/mysql') do
